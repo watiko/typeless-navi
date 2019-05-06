5b8d0c098ee5291bcc66efa334dc62b458b648cb
watiko <service@mail.watiko.net>
2019-05-06 15:28:45 +0900

    feat: redux-logger

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense } from 'react';
-import { initialize } from 'typeless';
 import { Router, View } from 'react-navi';
 
 import { navigation } from '@app/routes';
@@ -7,20 +6,16 @@ import { useRouterModule } from 'features/router/module';
 
 import { Layout } from './Layout';
 
-const { TypelessProvider } = initialize();
-
 export const App = () => {
   useRouterModule();
 
   return (
-    <TypelessProvider>
-      <Router navigation={navigation}>
-        <Suspense fallback={null}>
-          <Layout>
-            <View />
-          </Layout>
-        </Suspense>
-      </Router>
-    </TypelessProvider>
+    <Router navigation={navigation}>
+      <Suspense fallback={null}>
+        <Layout>
+          <View />
+        </Layout>
+      </Suspense>
+    </Router>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import { createEpicMiddleware, onHmr, RootEpic, RootReducer, TypelessProvider } from 'typeless';
 
-import { App } from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MOUNT_NODE = document.getElementById('root');
+
+if (!MOUNT_NODE) {
+  throw new Error('<div id="root" /> not found');
+}
+
+const rootEpic = new RootEpic();
+const rootReducer = new RootReducer();
+
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+const middlewares = [epicMiddleware];
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger').createLogger;
+  middlewares.push(
+    createLogger({
+      collapsed: true,
+    }),
+  );
+}
+export const store = createStore(rootReducer.getReducer(), applyMiddleware(...middlewares));
+
+const render = () => {
+  const App = require('./components/App').App;
+  ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+  ReactDOM.render(
+    <TypelessProvider rootEpic={rootEpic} rootReducer={rootReducer} store={store}>
+      <App />
+    </TypelessProvider>,
+    MOUNT_NODE,
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    onHmr(render);
+  });
+}
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
