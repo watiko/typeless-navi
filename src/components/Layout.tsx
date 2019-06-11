3f297ed518e3c6aeae04cfb4259f4c70ba865f83
watiko <service@mail.watiko.net>
2019-06-12 00:44:37 +0900

    fix: use Grommet's Container Compoent

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoadingRoute } from 'react-navi';
+import { Grommet } from 'grommet';
 
 import { Dashboard } from './Dashboard';
 import { Spinner } from './Spinner';
@@ -12,8 +13,8 @@ export const Layout = ({ children }: Props) => {
   const loadingRoute = useLoadingRoute();
 
   return (
-    <div>
+    <Grommet>
       <Dashboard>{!loadingRoute ? children : <Spinner />}</Dashboard>
-    </div>
+    </Grommet>
   );
 };
